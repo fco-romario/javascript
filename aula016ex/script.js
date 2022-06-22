5fa35c60d40f47040a3ed20b8cfb8b798c665bab
romariofc <106566259+fco-romario@users.noreply.github.com>
2022-06-22 20:34:25 -0300

    eula16ex.  listador de números.

---
+++ aula016ex/script.js
@@ -0,0 +1,70 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >=  1 && Number(n) <= 100) {
+        return true
+    } else {
+       return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+            
+            
+        }
+        media = soma / tot
+        /*for (let posDois in valores){
+            soma += valores[posDois]
+        }
+        for (let posDois in valores){
+            
+        }*/
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos <strong>${tot} elementos</strong> cadastrado.</p>`
+        res.innerHTML += `<p>O <strong>maior</strong> valor informado foi <strong>${maior}</strong></p>`
+        res.innerHTML += `<p>O <strong>menor</strong> valor informado foi <strong>${menor}</strong></p>`
+        res.innerHTML += `<p>A <strong>soma</strong> de todos os valores foi:  <strong>${soma}</strong></p>`
+        res.innerHTML += `<p>A <strong>média</strong> de todos os valores foi:  <strong>${media}</strong></p>`
+    }
+}
