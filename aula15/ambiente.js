b952d7302f25087859de60e9f70be94259f268d5
romariofc <106566259+fco-romario@users.noreply.github.com>
2022-06-22 12:16:34 -0300

    Vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,8,2,9,3]
+console.log(num)
+num.sort()
+console.log(num)
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O priemiro valor do vetor é ${num[0]}`)
